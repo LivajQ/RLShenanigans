89fb7126570792df49b0e598885bbb296a19c9d4
LivajQ <leviackerman.pl@gmail.com>
2025-10-19 21:56:22 +0200

    Invasion timer fix
    Now ticking with players instead of relying on world time so that you don't instantly get invaded when playing on servers. And global cooldown to avoid spamming

--- src/main/java/rlshenanigans/handlers/NPCInvasionHandler.java
+++ src/main/java/rlshenanigans/handlers/NPCInvasionHandler.java
@@ -16,28 +16,36 @@ import rlshenanigans.entity.npc.EntityNPCInvader;
 import rlshenanigans.entity.npc.EntityNPCSummon;
 
 import java.util.List;
-import java.util.Random;
 import java.util.UUID;
 
 @Mod.EventBusSubscriber(modid = RLShenanigans.MODID)
 public class NPCInvasionHandler {
-    
+    private static final int GLOBAL_INVASION_COOLDOWN_MAX = 3600;
+    private static int globalInvasionCooldown = 0;
+
+    @SubscribeEvent
+    public static void onWorldTick(TickEvent.WorldTickEvent event) {
+        if (event.world.isRemote || event.phase == TickEvent.Phase.START || globalInvasionCooldown <= 0) return;
+        globalInvasionCooldown--;
+    }
+
     @SubscribeEvent
     public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
         EntityPlayer player = event.player;
         if (!ForgeConfigHandler.npc.invasionsEnabled
                 || player.world.isRemote
                 || player.capabilities.isCreativeMode
-                || player.ticksExisted < 6000
                 || player.ticksExisted % 200 != 0
                 || event.phase == TickEvent.Phase.START) return;
-        
         NBTTagCompound data = player.getEntityData();
-        long now = player.world.getTotalWorldTime();
-        if (!data.hasKey("NextInvasion")) data.setLong("NextInvasion", now + rollNextInvasion());
-        if (data.getLong("NextInvasion") < now && nearbyInvaders(player).isEmpty()) spawnInvader(player, 100);
+        long nextInvasion = data.hasKey("NextInvasion") ? data.getLong("NextInvasion") : rollNextInvasion();
+        if (globalInvasionCooldown <= 0 && nextInvasion <= 0 && nearbyInvaders(player).isEmpty()) spawnInvader(player, 100);
+        else if (nextInvasion > 0) {
+            nextInvasion -= 200;
+            data.setLong(("NextInvasion"), nextInvasion);
+        }
     }
-    
+
     @SubscribeEvent
     public static void onInvaderDeath(LivingDeathEvent event) {
         if (!ForgeConfigHandler.npc.invasionsEnabled) return;
@@ -48,7 +56,7 @@ public class NPCInvasionHandler {
         if (player == null) return;
         
         double distanceSq = invader.getDistanceSq(player);
-        if (distanceSq > 128 * 128) player.getEntityData().setLong("NextInvasion", player.world.getTotalWorldTime() + rollNextInvasion());
+        if (distanceSq < 128 * 128) player.getEntityData().setLong("NextInvasion", rollNextInvasion());
     }
     
     @SubscribeEvent
@@ -56,12 +64,11 @@ public class NPCInvasionHandler {
         if (!ForgeConfigHandler.npc.invasionsEnabled) return;
         if (!(event.getEntity() instanceof EntityPlayer)) return;
         EntityPlayer player = (EntityPlayer) event.getEntity();
-        long now = player.world.getTotalWorldTime();
-        if (player.getEntityData().getLong("NextInvasion") >= now) return;
+        if (player.getEntityData().getLong("NextInvasion") > 0) return;
         
         List<EntityNPCInvader> invaders = nearbyInvaders(player);
         boolean matched = invaders.stream().anyMatch(invader -> player.getUniqueID().equals(invader.getInvadedPlayerUUID()));
-        if (matched) player.getEntityData().setLong("NextInvasion", now + rollNextInvasion());
+        if (matched) player.getEntityData().setLong("NextInvasion", rollNextInvasion());
     }
     
     @SubscribeEvent
@@ -89,6 +96,7 @@ public class NPCInvasionHandler {
         if (!invader.spawnInRadius(invader.world, playerPos, 24, 32, false)) return;
         DifficultyInstance difficulty = player.world.getDifficultyForLocation(playerPos);
         invader.onInitialSpawn(difficulty, null);
+        globalInvasionCooldown = GLOBAL_INVASION_COOLDOWN_MAX;
     }
     
     private static List<EntityNPCInvader> nearbyInvaders(EntityPlayer player) {
@@ -105,6 +113,6 @@ public class NPCInvasionHandler {
         int offset = ForgeConfigHandler.npc.invasionOffset;
         int min = Math.max(0, base - offset);
         int max = base + offset;
-        return min + new Random().nextInt(max - min + 1);
+        return min + RLShenanigans.RLSRAND.nextInt(max - min + 1);
     }
 }
